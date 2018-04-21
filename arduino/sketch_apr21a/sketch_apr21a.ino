53bdcd7aa637582470e30545fdf7a0a51753bb35
Kevin Chang <kevin.chang.1998@outlok.com>
2018-04-21 16:15:36 -0700

    added arduino testing code

--- arduino/sketch_apr21a/sketch_apr21a.ino
+++ arduino/sketch_apr21a/sketch_apr21a.ino
@@ -1,7 +1,35 @@
+#include <Servo.h>
+
+#define SERVO_PIN 9
+
+Servo servo;
+char cat;
+
 void setup() {
   Serial.begin(115200); // use the same baud-rate as the python side
+  servo.attach(SERVO_PIN); // connects the servo
+  servo.write(90); // default
 }
+
 void loop() {
-  Serial.println("SERIAL IS AVAILABLE"); // write a string
+  if (Serial.available()) { // if there's data to read
+    cat = (char) Serial.read();
+    servoExe(); // executes the servo
+  }
+  Serial.println("SERIAL IS AVAILABLE"); // writes a string
   delay(1000);
 }
+
+// executes the servo
+void servoExe() {
+  if (cat == 'R') { // recycle
+    servo.write(0); // sets the angle of servo
+  } else if (cat == 'L') { // landfill
+    servo.write(90); 
+  } else if (cat == 'C') { // compost
+    servo.write(180); 
+  }
+  delay(4000);
+  servo.write(90);
+}
+

7d910a81be0c89efba0a04d84181bc48007693e6
Tejasvi Nareddy <tejunareddy@gmail.com>
2018-04-22 02:51:31 -0700

    Pull realtime firebase data and store in state

--- react-page/src/app.jsx
+++ react-page/src/app.jsx
@@ -14,6 +14,8 @@ import React from "react";
 //simply by importing it's module name
 import "whatwg-fetch";
 
+import firebase from 'firebase'
+
 //import our CSS file
 //Webpack will actually merge the contents
 //of this file into an inline <style></style>
@@ -26,16 +28,53 @@ import "whatwg-fetch";
 //directory 
 //import "./css/main.css";
 
-import Map from "./map.jsx";
-
 export default class extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            data: {
-                items: []
-            }
+
+        const self = this;
+
+        // Initialize Firebase
+        let config = {
+            apiKey: "AIzaSyDLxi0Uiyzdt6Qkjh4wX3uVxWKYn4YdsBQ",
+            authDomain: "smarttrashcan-a731b.firebaseapp.com",
+            databaseURL: "https://smarttrashcan-a731b.firebaseio.com",
+            projectId: "smarttrashcan-a731b",
+            storageBucket: "smarttrashcan-a731b.appspot.com",
+            messagingSenderId: "644066905865"
         };
+        firebase.initializeApp(config);
+
+        // Get a reference to the database service
+        let database = firebase.database();
+
+        let previous_items = [];
+        let current_items = [];
+
+        let starCountRef = database.ref('/');
+        starCountRef.on('value', function(snapshot) {
+            let root_map = snapshot.val();
+            for (let i = 0; i < Object.keys(root_map).length; i++) {
+              let can_id = Object.keys(root_map)[i];
+              let current_fullness = root_map[can_id].current_fullness;
+              current_fullness.can_id = can_id;
+              current_items.push(current_fullness);
+            }
+
+            if (current_items === previous_items) {
+              return;
+            }
+            previous_items = current_items;
+            current_items = [];
+
+            self.state = {
+              data: {
+                items: previous_items
+              }
+            };
+
+            console.log(self.state);
+        });
     }
 
     render() {

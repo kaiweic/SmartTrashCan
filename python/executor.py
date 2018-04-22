92e5043f559b0ed406a34b4d6c0a5e0da049577e
SujeethJinesh <sujeethjinesh@gmail.com>
2018-04-21 23:36:44 -0700

    Add Firebase

--- python/executor.py
+++ python/executor.py
@@ -1,8 +1,10 @@
 from python.clarifai_logic import get_items_in_picture
+from python.firebase import initialize_firebase
 from python.picture_logic import take_picture
 
 
 def main():
+    initialize_firebase()
     picture_name = take_picture()
     get_items_in_picture(picture_name)
 
